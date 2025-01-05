65779a319b7607a4bac52c87c31da1245f0c1a88
Jakub Ziębiński <ziebinskijakub@gmail.com>
2025-01-05 22:03:15 +0100

    Implementation of the constants package

---
+++ car/core/constants/timeouts.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from typing import Final
+
+
+DEFAULT_WAIT_TIMEOUT: Final[int] = 10
---
+++ car/core/constants/urls.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from typing import Final
+
+
+OLX_URL: Final[str] = "https://www.olx.pl/"
