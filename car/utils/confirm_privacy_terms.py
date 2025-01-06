058f099a018f53bb8eff1dd8055df1da3a5e0cb6
Jakub Ziębiński <ziebinskijakub@gmail.com>
2025-01-06 11:01:29 +0100

    Implementation of the function to confirm  privacy terms in the olx

--- car/utils/confirm_privacy_terms.py
+++ car/utils/confirm_privacy_terms.py
@@ -6,15 +6,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-from car.car.core.constants import CONFIRM_PRIVACY_TERMS_BUTTON_ID, DEFAULT_WAIT_TIMEOUT
+from car.car.core.constants import (
+    CONFIRM_PRIVACY_TERMS_BUTTON_ID_OLX,
+    DEFAULT_WAIT_TIMEOUT,
+)
 
 if TYPE_CHECKING:
     from selenium.webdriver import Chrome
 
 
 def confirm_olx_privacy_terms(webdriver: Chrome) -> None:
-    """Confirm privacy tems when entering OLX."""
+    """Confirm privacy terms when entering OLX."""
     WebDriverWait(webdriver, DEFAULT_WAIT_TIMEOUT).until(
-        EC.presence_of_element_located((By.ID, CONFIRM_PRIVACY_TERMS_BUTTON_ID))
+        EC.presence_of_element_located((By.ID, CONFIRM_PRIVACY_TERMS_BUTTON_ID_OLX))
     )
-    webdriver.find_element(By.ID, CONFIRM_PRIVACY_TERMS_BUTTON_ID).click()
+    webdriver.find_element(By.ID, CONFIRM_PRIVACY_TERMS_BUTTON_ID_OLX).click()
