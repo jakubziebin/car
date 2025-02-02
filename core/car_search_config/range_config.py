d1398534f93329c6587bae399905330d71a6e5fc
Jakub Ziębiński <ziebinskijakub@gmail.com>
2025-02-02 22:04:03 +0100

    Update setting ranges input in the olx to use base range inputs setter

---
+++ core/car_search_config/range_config.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from datetime import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class CarRangesConfig:
+    """Config class for car search ranges."""
+
+    min_year: int | None = None
+    max_year: int = datetime.now().year
+    max_price: int | None = None
+    min_capacity: int | None = None
+    max_capacity: int | None = None
+    min_mileage: int | None = None
+    max_mileage: int | None = None
+    min_power: int | None = None
+    max_power: int | None = None
--- hello.py
+++ hello.py
@@ -23,33 +23,30 @@ from car.utils.olx.set_dropdown_inputs import (
     set_car_steering_wheel_placement_olx,
     set_car_technical_condition_olx,
 )
-from car.utils.olx.set_search_ranges import (
-    set_max_price_olx,
-    set_engine_capacity_olx,
-    set_car_mileage_olx,
-    set_production_year_fork_olx,
-    set_engine_power_olx,
-)
-
-
-def main():
-    print("Hello from car!")
+from car.utils.olx.set_search_ranges import OlxRangeInputsSetter
+from car.core.car_search_config.range_config import CarRangesConfig
 
 
 if __name__ == "__main__":
     service = Service(executable_path=CHROME_DRIVER_PATH)
     driver = webdriver.Chrome(service=service)
     driver.get(OLX_URL)
+
+    ranges_config = CarRangesConfig(
+        max_price=40_000,
+        min_year=2014,
+        max_year=2020,
+        min_capacity=1250,
+        max_mileage=100_000,
+        min_power=85,
+    )
+
     confirm_olx_privacy_terms(driver)
     paste_car_model_in_olx_search(driver, "Kia", "rio")
     confirm_search_olx(driver)
-    set_max_price_olx(driver, 40_000)
     set_category_olx(driver)
     choose_car_model_olx(driver, "rio")
-    set_production_year_fork_olx(driver, from_year=2014, to_year=2018)
-    set_engine_capacity_olx(driver, from_capacity=1250)
-    set_car_mileage_olx(driver, max_mileage=100_000)
-    set_engine_power_olx(driver, min_power=85)
+    OlxRangeInputsSetter(driver, ranges_config).execute_range_inputs_filling()
     set_fuel_type_olx(driver, "Benzyna")
     set_drive_type_olx(driver, "Na przednie koła")
     set_gearbox_type_olx(driver, "Manualna")
