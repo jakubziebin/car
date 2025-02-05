a7311558de4fdf4dea43b58438ecd4048bc02cd6
Jakub Ziębiński <ziebinskijakub@gmail.com>
2025-02-05 18:12:34 +0100

    Implementation of the stale element handler

---
+++ utils/stale_element_handle.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Callable, ClassVar, TypeAlias
+
+from selenium.common.exceptions import StaleElementReferenceException
+
+from car.exceptions.car_exception import CarException
+
+
+class StaleElementMaxTriesExceedError(CarException):
+    """Raises when tried to find stale element more than `max_tries` times."""
+
+
+CallableToPreventStaleElement: TypeAlias = Callable[..., bool]
+
+
+class StaleElementHandler:
+    MAX_TRIES: ClassVar[int] = 3  # override to modify
+
+    def __init__(self, executable: CallableToPreventStaleElement) -> None:
+        """
+        Initialize StaleElementHandler.
+
+        Args:
+            executable: function to execute in the handler. Must return bool either passed or not.
+        """
+        self._executable = executable
+
+    def execute(self) -> None:
+        """Tries to execute `element_click_function` `max_tries` times."""
+        current = 0
+        exception: StaleElementReferenceException | None = None
+
+        while current <= self.MAX_TRIES:
+            try:
+                result = self._executable()
+                if result:
+                    return
+            except StaleElementReferenceException as error:
+                current += 1
+                exception = error
+                continue
+
+        raise StaleElementMaxTriesExceedError from exception
+
+    def switch_executable(self, new_executable: CallableToPreventStaleElement) -> None:
+        """Switches function to execute"""
+        self._executable = new_executable
